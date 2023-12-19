d54349896f3ce56749579f0a53d9cea1e2583d74
Tarasdeveloper <107717399+Tarasdeveloper@users.noreply.github.com>
2023-12-19 08:51:38 +0000

    Deploying to gh-pages from @ Tarasdeveloper/tb-care@ba2c4154425041f3a7dd05ccc6c1e3663214fda6 🚀

---
+++ index.b4c20201.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){const e=document.querySelector(".header-menu_btn"),n=document.querySelector(".aside_menu-btn"),t=document.querySelector(".aside_menu");e.addEventListener("click",(function(){t.classList.toggle("show")})),n.addEventListener("click",(function(){t.classList.remove("show")}))}));
+//# sourceMappingURL=index.b4c20201.js.map
---
+++ index.d60d82b0.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var e=document.querySelector(".header-menu_btn"),n=document.querySelector(".aside_menu-btn"),t=document.querySelector(".aside_menu");e.addEventListener("click",(function(){t.classList.toggle("show")})),n.addEventListener("click",(function(){t.classList.remove("show")}))}));
+//# sourceMappingURL=index.d60d82b0.js.map
